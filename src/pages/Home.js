541cddc587bee7eb94dfe2c686e0de9fe3e2bf0c
GiulianoGiovanola <giulianogiovanola@gmail.com>
2022-11-17 01:19:34 -0300

    actualizacion

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,20 @@
 import React from 'react';
 import Hero from '../components/Hero/Hero';
 import Navbar from '../components/Navbar/Navbar';
+import Collection from '../components/Collection/Collection';
 import MintGeneral from '../components/MintGeneral/MintGeneral';
+import About from '../components/About/About';
 import Footer from '../components/Footer/Footer';
+import About from '../components/About/About';
 
 const Home = () => {
   return (
     <>
       <Navbar />
       <Hero />
+      <Collection />
       <MintGeneral />
+      <About />
       <Footer />
     </>
   );
