90725051b298cfe36f63c7dcc3f0504cbeb685e2
GiulianoGiovanola <giulianogiovanola@gmail.com>
2022-11-17 14:08:11 -0300

    actualizacion de navbar

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -5,43 +5,43 @@ import WalletButton from "../WalletButton/WalletButton";
 const Navbar = () => {
   return (
     <>
-      <div>
-        <div className="divheader">
+      <div className={styles["container"]}>
+        <div className={styles["divheader"]}>
           <div>
-            <a>
               <img
                 src="assets/logo.png"
                 width=""
                 height=""
                 alt="logo-persona"
               />
-            </a>
           </div>
-          <nav className="intro">
-            <ul>
-              <li>
-                <a href="#Collection" id="Check_Collection">
-                  Collection
-                </a>
-              </li>
-              <li>
-                <a href="#Mint" id="Mint_it">
-                  Mint
-                </a>
-              </li>
-              <li>
-                <a href="#Faq" id="FAQ">
-                  FAQ
-                </a>
-              </li>
-              <li>
-                <a href="#About" id="About_us">
-                  About
-                </a>
-              </li>
-            </ul>
-          </nav>
-          <WalletButton />
+          <div className={styles["containeroptions"]}>
+            <nav>
+              <ul className={styles["intro"]}>
+                <li>
+                  <a href="#Collection" className={styles["options"]}>
+                    Collection
+                  </a>
+                </li>
+                <li>
+                  <a href="#Mint" className={styles["options"]}>
+                    Mint
+                  </a>
+                </li>
+                <li>
+                  <a href="#Faq" className={styles["options"]}>
+                    FAQ
+                  </a>
+                </li>
+                <li>
+                  <a href="#About" className={styles["options"]}>
+                    About
+                  </a>
+                </li>
+              </ul>
+            </nav>
+            <WalletButton />
+          </div>
         </div>
       </div>
     </>
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,21 +1,29 @@
-import React from 'react';
-import Hero from '../components/Hero/Hero';
-import Navbar from '../components/Navbar/Navbar';
-import Collection from '../components/Collection/Collection';
-import MintGeneral from '../components/MintGeneral/MintGeneral';
-import About from '../components/About/About';
-import Footer from '../components/Footer/Footer';
+import React from "react";
+import Hero from "../components/Hero/Hero";
+import Navbar from "../components/Navbar/Navbar";
+import Collection from "../components/Collection/Collection";
+import MintGeneral from "../components/MintGeneral/MintGeneral";
+import About from "../components/About/About";
+import Footer from "../components/Footer/Footer";
+import background from "../assets/background.jpg";
 
 const Home = () => {
   return (
-    <>
+    <div
+      style={{
+        backgroundImage: `url(${background})`,
+        backgroundRepeat: "no-repeat",
+        backgroundAttachment: "fixed",
+        backgroundSize: "cover"
+      }}
+    >
       <Navbar />
       <Hero />
       <Collection />
       <MintGeneral />
       <About />
       <Footer />
-    </>
+    </div>
   );
 };
 
