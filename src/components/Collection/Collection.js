dec1fc98611c67ab01fdbf484a4fbb1d7a004832
GiulianoGiovanola <giulianogiovanola@gmail.com>
2022-11-23 17:17:17 -0300

    actualizacion collection

--- src/components/Collection/Collection.js
+++ src/components/Collection/Collection.js
@@ -6,11 +6,18 @@ import styles from "./Collection.module.css"
 const Collection = ({ dataimg }) => {
   
   return (
+    <>
     <Marquee gradient={false}>
       {dataimg.map((img, index) => {
         return (<img className={styles["imgNFT"]} src={img.src} alt={img.alt} key={index} />);
       })}
     </Marquee>
+    <Marquee gradient={false} direction={"right"}>
+    {dataimg.map((img, index) => {
+      return (<img className={styles["imgNFT"]} src={img.src} alt={img.alt} key={index} />);
+    })}
+  </Marquee>
+    </>
   );
 };
 
