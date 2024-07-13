fa7024eb3226b96961a6a127f8eeea34b93fe7a8
unknown <giulianogiovanola@gmail.com>
2024-07-13 17:56:20 -0300

    test 2024

--- src/components/FAQ/FAQ.js
+++ src/components/FAQ/FAQ.js
@@ -6,20 +6,20 @@ const data = {
   title: "How can we help?",
   rows: [
     {
-      title: "Lorem ipsum dolor sit amet,",
-      content: "Lorem ipsum dolor sit amet, consectetur "
+      title: "What is Pink Rabbit NFT?",
+      content: "Pink Rabbit is a one-of-a-kind NFT depicting a digitally designed pink rabbit. Each Pink Rabbit token is backed by high-quality digital art and is part of a limited collection, making it a coveted piece within the NFT space."
     },
     {
-      title: "Nunc maximus, magna at ultricies elementum",
-      content: "Nunc maximus, magna at ultricies elementum, risus turpis vulputate quam."
+      title: "What is the Pink Rabbit NFT release date?",
+      content: "The Pink Rabbit NFT will be released on April 27, 2025 and is available for purchase from then on."
     },
     {
-      title: "Curabitur laoreet, mauris vel blandit fringilla",
-      content: "Curabitur laoreet, mauris vel blandit fringilla, leo elit rhoncus nunc"
+      title: "What are the main features of Pink Rabbit NFT?",
+      content: "Pink Rabbit is an exclusive NFT backed by high-quality digital art, designed for collectors who value originality and scarcity. Each token features unique digital art created by leading artists, guaranteeing its authenticity and artistic value. Featuring a limited edition, Pink Rabbit was initially released on July 1, 2023, making it a sought-after piece in the NFT world, with plans for future releases based on demand and the popularity of the project."
     },
     {
-      title: "What is the package version",
-      content: "v1.0.5"
+      title: "Where can I learn more about Pink Rabbit NFT?",
+      content: "For more details about Pink Rabbit NFT, including news and future releases, you can visit our social media and OpenSea where the NFT will be launched."
     }]
 }
 
