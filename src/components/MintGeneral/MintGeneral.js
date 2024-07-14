9e57bdb78380066eba558dd583c04328038e92fb
unknown <giulianogiovanola@gmail.com>
2024-07-13 22:45:03 -0300

    test 2024

--- src/components/MintGeneral/MintGeneral.js
+++ src/components/MintGeneral/MintGeneral.js
@@ -4,7 +4,7 @@ import WalletClient from "../WalletClient/WalletClient";
 const MintGeneral = () => {
   return (
     <>
-      <div id="Mint" className="homeContainer">
+      <div id="Mint" className="homeContainer" style={{ margin: '2em 0' }}>
         <h1>Minimal Mint</h1>
         <div className="modulesContainer">
           <WalletClient />
